b7f6cd0e80153fead55363502eec07390a55024f
Joel King <king4ja@mail.uc.edu>
2023-01-18 18:03:07 -0500

    Added some button layouts in the top part of the page, made the google maps bigger with a search function and add a photo placement for  the apartments will look on the side

--- VSL coding/VSL.js
+++ VSL coding/VSL.js
@@ -28,80 +28,87 @@ User.findOne({username:"hi"}, (err, foundItem) =>{
     }else{
         console.log(foundItem);
     }
-});
-
-function initMap(){
-    //Map options
-    var options ={
-        zoom: 14,
-        center: {lat:39.1320, lng: -84.5155}
-    }
-
-
-    //New map
-    var map = new google.maps.Map(document.getElementById("map"), options);
-
-    //Listen for click on map
-    google.maps.event.addListener(map, 'click', function(event){
-        addMarker({coords:event.latLng});
     });
 
-    /*
-    //Add Marker
-    var marker = new google.maps.Marker({
-        position:{lat:39.1320 , lng:  -84.5155 },
-        map: map
+
+function initAutocomplete() {
+        const map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat:39.1320, lng: -84.5155},
+        zoom: 15,
+        mapTypeId: "roadmap",
+    });  
+       
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
     });
-
-    //Information Popup on Location
-    var infoWindow = new google.maps.InfoWindow({
-        content:'<h1>UC Campus</h1>'
+  
+    let markers = [];
+  
+    // Listen for the event fired when the user selects a prediction and retrieve
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+  
+      if (places.length == 0) {
+        return;
+      }
+  
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+  
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+  
+      places.forEach((place) => {
+        if (!place.geometry || !place.geometry.location) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+  
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+  
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            icon,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
     });
+  }
+  
+  window.initAutocomplete = initAutocomplete;
 
-    marker.addListener('click', function(){
-        infoWindow.open(map, marker);
-    });*/
+    
 
-    // Array of Markers
-
-    var markers = [
-        {coords:{lat:39.1320, lng: -84.5155},
-        content:'<h1>UC Campus</h1>'}
-
-    ];
-
-    //Loop Through markers
-    for(var i =0;i < markers.length;i++){
-        aadMarker(markers[i]);
-
-         addMarker({coords:{lat:39.1320, lng: -84.5155},
-        content:'<h1>UC Campus</h1>'
-        });
-
-    };
-
+    
    
-    // Add Marker Function
-    function addMarker(props){
-        var marker = new google.maps.Marker({
-            position: props.coords,
-            map: map
-        });
-    };
 
-    //Check content
-    if(props.content){
-         //Information Popup on Location
-        var infoWindow = new google.maps.InfoWindow({
-            content: props.content
-        });
 
-        marker.addListener('click', function(){
-            infoWindow.open(map, marker);
-        });
-    };
-
-}
 
 
 app.listen(3000, function(){
